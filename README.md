30593f3905b3af37c6317e96826eb69e80fdeb75
Heavenston <malolegendrelemaire@@gmail.com>
2023-04-05 21:06:49 +0100

    Copied crates docs to README

---
+++ README.md
@@ -0,0 +1,17 @@
+# debug_unwrap
+
+Adds the method debug_unwrap for when you just want to make it work.
+
+This library adds the [DebugUnwrap](DebugUnwrap) extension trait that adds
+the method [debug_unwrap](DebugUnwrap::debug_unwrap) to the
+[Option](std::option::Option) and [Result](std::result::Result) types.
+It does exactly the same thing as the normal unwrap methods, but won't exist
+when compiling without debug_assertions enabled
+(i.e. when not compiling in Debug mode).
+
+There is also three other aliases that all have there respective
+library features of the same name to enable them:
+ - out (enabled by default)
+ - o
+ - peel
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,4 @@
-//! Adds the method debug_unwrap for when you just want to make it work.
-//!
-//! This library adds the [DebugUnwrap](DebugUnwrap) extension trait that adds
-//! the method [debug_unwrap](DebugUnwrap::debug_unwrap) to the
-//! [Option](std::option::Option) and [Result](std::result::Result) types.
-//! It does exactly the same thing as the normal unwrap methods, but won't exist
-//! when compiling without debug_assertions enabled
-//! (i.e. when not compiling in Debug mode).
-//!
-//! There is also three other aliases that all have there respective
-//! library features of the same name to enable them:
-//!  - out (enabled by default)
-//!  - o
-//!  - peel
+#![doc = include_str!("../README.md")]
 use std::fmt::Debug;
 
 /// Extension trait for the Result and Option types, see crate-level
